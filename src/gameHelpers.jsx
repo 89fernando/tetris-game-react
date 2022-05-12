79f9face876b25b01485c01711aebdfce0d2ddb5
89fernando <fernando89soares@gmail.com>
2022-05-11 23:02:25 -0300

    Create new hooks and clear rows

--- src/gameHelpers.jsx
+++ src/gameHelpers.jsx
@@ -9,13 +9,12 @@ export const createStage = () =>
 export const checkCollision = (player, stage, { x: moveX, y: moveY }) => {
   for (let y = 0; y < player.tetromino.length; y += 1) {
     for (let x = 0; x < player.tetromino[y].length; x += 1) {
-      // 1. Check that we're on an actual tetromino cell
+ 
       if (player.tetromino[y][x] !== 0) {
         if (
-          // 2. Check that our move is inside the game area
+
           !stage[y + player.pos.y + moveY] ||
           !stage[y + player.pos.y + moveY][x + player.pos.x + moveX] ||
-          // 3. Check that we're not colliding with a fixed block
           stage[y + player.pos.y + moveY][x + player.pos.x + moveX][1] !== 'clear'
         ) {
           return true;
